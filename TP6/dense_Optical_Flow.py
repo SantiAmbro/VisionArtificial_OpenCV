23c5ceba4964f3939cac2137d508799e5186bd7c
santiambro <santiago.ambrosetti@ing.autral.edu.ar>
2019-04-29 18:16:57 -0300

    Kalman filter with Camshift

--- TP6/dense_Optical_Flow.py
+++ TP6/dense_Optical_Flow.py
@@ -1,34 +1,71 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture("vtest.avi")
-ret, frame1 = cap.read()
-prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
-#Return an array of zeros with the same shape and type as a given array.
-hsv = np.zeros_like(frame1)
-hsv[..., 1] = 255
-while True:
-    ret, frame = cap.read()
-    next = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #Computes a dense optical flow using the Gunnar Farneback's algorithm.
-    flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    #Calculates the magnitude and angle of 2D vectors.
-    mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
-    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
-    cv.imshow('frame', frame)
-    cv.imshow('frame2', bgr)
+def video():
+    cap = cv.VideoCapture("vtest.avi")
+    ret, frame1 = cap.read()
+    prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+    #Return an array of zeros with the same shape and type as a given array.
+    hsv = np.zeros_like(frame1)
+    hsv[..., 1] = 255
+    while True:
+        ret, frame = cap.read()
+        next = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        #Computes a dense optical flow using the Gunnar Farneback's algorithm.
+        flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        #Calculates the magnitude and angle of 2D vectors.
+        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang * 180 / np.pi / 2
+        hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+        bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+        cv.imshow('frame', frame)
+        cv.imshow('frame2', bgr)
 
-    k = cv.waitKey(30) & 0xff
+        k = cv.waitKey(30) & 0xff
 
-    if k == ord('q'):
-        break
-    elif k == ord('s'):
-        cv.imwrite('opticalfb.png', frame)
-        cv.imwrite('opticalhsv.png', bgr)
+        if k == ord('q'):
+            break
+        elif k == ord('s'):
+            cv.imwrite('opticalfb.png', frame)
+            cv.imwrite('opticalhsv.png', bgr)
 
-    prvs = next
+        prvs = next
 
-cap.release()
-cv.destroyAllWindows()
+    cap.release()
+    cv.destroyAllWindows()
+
+def webcam():
+    cap = cv.VideoCapture(0)
+    ret, frame1 = cap.read()
+    prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+    #Return an array of zeros with the same shape and type as a given array.
+    hsv = np.zeros_like(frame1)
+    hsv[..., 1] = 255
+    while True:
+        ret, frame = cap.read()
+        next = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        #Computes a dense optical flow using the Gunnar Farneback's algorithm.
+        flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        #Calculates the magnitude and angle of 2D vectors.
+        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang * 180 / np.pi / 2
+        hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+        bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+        cv.imshow('frame', frame)
+        cv.imshow('frame2', bgr)
+
+        k = cv.waitKey(30) & 0xff
+
+        if k == ord('q'):
+            break
+        elif k == ord('s'):
+            cv.imwrite('opticalfb.png', frame)
+            cv.imwrite('opticalhsv.png', bgr)
+
+        prvs = next
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+webcam()
